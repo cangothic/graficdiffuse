d01adb9927766d9121f240e2eb53aeb869a2d5a2
zhuvry <briangiraldo123@hotmail.com>
2016-04-04 04:33:16 -0500

    arreglo polimorfismo nanana batman

--- graficadora/main.cpp
+++ graficadora/main.cpp
@@ -11,10 +11,15 @@ int main(int argc, char * args[]){
     function<double (double)> funcionPrueba = retornarTrapesoidal(1,3,4,5);
     EscenaGraficadora escena(funcionPrueba,"grafica");
 
-    Escena x = (Escena)escena;
-    escena.actualizar();
+    //el parametro 0 es el path del exe
+    //descomentar si vas a usarlos!! si no explota boom!
+    //string parametro1 = args[1]; //parametro 1
+    //string parametro2 = args[2]; //parametro 2
+
+    Escena* x = &escena;
+    x->actualizar();
     while(true){
-        escena.dibujar();
+        x->dibujar();
     }
 //
 //    Director* director = Director::getInstance();
